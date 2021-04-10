2dd64e03f8d593a3d6e7a06d9337f20e606d14a1
Nitu Kumari <79151737+Ni2-1911@users.noreply.github.com>
2021-04-10 23:01:17 +0530

    Update prettified_html_text.py

--- prettified_html_text.py
+++ prettified_html_text.py
@@ -4,14 +4,22 @@ from bs4 import BeautifulSoup
 
 
 def get_source_code(url):
-    r = requests.get(url)
+    header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0", 
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8", 
+        "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", 
+        "Connection": "close", "Upgrade-Insecure-Requests": "1"}  
+    r = requests.get(url,headers = header)
     htmlContent = r.content
 
     soup = BeautifulSoup(htmlContent, 'html.parser')
     print(soup.prettify)
 
 def get_page_text(url):
-    r = requests.get(url)
+    header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0", 
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8", 
+        "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", 
+        "Connection": "close", "Upgrade-Insecure-Requests": "1"}  
+    r = requests.get(url,headers = header)
     htmlContent = r.content
 
     soup = BeautifulSoup(htmlContent, 'html.parser')
@@ -22,5 +30,3 @@ def get_page_text(url):
 if __name__ == "__main__":
     url = input("ENTER YOUR TARGET URL HERE : ")  # -- enter url here
     get_page_text(url)
-
-
