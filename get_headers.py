289aaacff4c0b837049799572973c2cccf13ff2f
Nitu Kumari <79151737+Ni2-1911@users.noreply.github.com>
2021-04-10 22:14:26 +0530

    Update get_headers.py

--- get_headers.py
+++ get_headers.py
@@ -1,11 +1,69 @@
-#get headers of requested site
 import requests
 from bs4 import BeautifulSoup
+import sys
+import re
+import threading
+import time
+from termcolor import colored
+import get_head
+global ux   
+ux=""
 
-def get_headers(url):   
-    response = requests.get(url) 
-    print(response.headers)
+initial_time = time.time()
+def get_links(u):
+    global ux,x
+    x=[]
+    for links in u:
+        try :
+            if("http" not in links and "https" not in links and links[0]== "/" and links != ""):
+                links=ux+links
+            elif("http" not in links and "https" not in links and links[0] !="/" and links != ""):
+                links=ux+"/"+links
+        except TypeError :
+            pass    
+        
+        task(links)
+                
+        try:
+            header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0", 
+                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8", 
+                    "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", 
+                    "Connection": "close", "Upgrade-Insecure-Requests": "1"}  
+        
+            response = requests.get(links, headers = header)
+            soup = BeautifulSoup(response.text,'html.parser')
 
-if __name__ == "__main__":
-    url = input("ENTER YOUR TARGET URL HERE : ")  # -- enter url here
-    get_headers(url)
+            for link in soup.find_all('a'):
+                x.append(link.get('href'))
+        except:
+            pass
+
+    return(x)
+
+def task(task):
+            
+    try:
+        print(get_head.get_headers(task))
+        print('WEBSITE: ' ,colored(task,'red'))
+        print('TIME : ', colored(time.time() - initial_time,'blue'))
+    except requests.exceptions.MissingSchema:
+        pass
+            
+            
+
+def get_depth(d,url):
+    text=[]
+    for i in range(d+1):
+      url = get_links(url)
+
+
+def get_url_depth(u ,d):
+    url=[]
+    url.append(u)
+    ux=url[0]
+    depth = int(d)
+    get_depth(depth,url)
+
+
+#example
+#get_url_depth('https://youtube.com',1)
