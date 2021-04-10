1d157f823103695609957013deb1250186bc6f88
Nitu Kumari <79151737+Ni2-1911@users.noreply.github.com>
2021-04-10 22:11:31 +0530

    Update Main_Tool.py

--- Main_Tool.py
+++ Main_Tool.py
@@ -9,57 +9,53 @@ from termcolor import colored
 
 #python files
 import _file_
-import get_phone_numbers
-import images_file_cnt
 import prettified_html_text
 import take_screenshot
-import get_head
-import get_all_links
-import sys
-import get_email
+#import get_phone_numbers
+import phone_depth
+#import images_file_cnt
+import img_cnt_depth
+#import get_email
+import email_depth
+#import get_all_links
 import _recurssion_
+#import get_head
+import get_headers
 
 
 
 def find(args):
 #url
 
-    if args.e == "head" and args.edit == "save":
+    if args.e == "head" and args.edit == "save" :
         print(colored("Headers are getting saved in :  ",'blue')+colored( args.path ,'red'))
         sys.stdout = open(args.path, "w")
-        get_head.get_headers(args.url)
+        get_headers.get_url_depth(args.url,args.depth)
         sys.stdout.close()
-    elif args.e == "head":
-        get_head.get_headers(args.url)
-    
+    elif args.e == "head" :
+        get_headers.get_url_depth(args.url,args.depth)
+
+        
     if args.e == "ss":
         take_screenshot.take_screenshot(args.url)
         print(colored("screenshot of this page is saved in current directory named :  ",'blue') + colored("main_page.png",'red' ))
-
-    if args.e == "link_d1" and args.edit == "save":
-        print(colored("links of depth_1 are getting saved in :  ",'blue')+ colored( args.path ,'red'))
-        sys.stdout = open(args.path, "w")
-        get_all_links.get_links(args.url)
-        sys.stdout.close() 
-    elif args.e == "link_d1":
-        get_all_links.get_links(args.url)
         
     if args.e == "phone_no" and args.edit == "save":
         print(colored("phone_no are getting saved in :  ",'blue')+colored( args.path,'red' ))
         sys.stdout = open(args.path, "w")
-        get_phone_numbers.get_phone(args.url)
+        phone_depth.get_url_depth(args.url , args.depth)
         sys.stdout.close()
     elif args.e == 'phone_no':
-        get_phone_numbers.get_phone(args.url)
+        phone_depth.get_url_depth(args.url , args.depth)
     
     if args.e == "email" and args.edit == "save":
         print(colored("emails are getting saved in :  ",'blue')+colored( args.path,'red' ))
         sys.stdout = open(args.path, "w")
-        get_email.get_source_code(args.url)
+        email_depth.get_url_depth(args.url,args.depth)
         sys.stdout.close()
     elif args.e == 'email':
-        get_email.get_source_code(args.url)
-
+        email_depth.get_url_depth(args.url,args.depth)
+        
     if args.e == 'link' and args.edit == 'save':
         print(colored("links are getting saved in :  ",'blue')+colored( args.path,'red' ))
         sys.stdout = open(args.path, "w")
@@ -71,10 +67,10 @@ def find(args):
     if args.e == "cnt_image" and args.edit == "save":
         print(colored("data for number of images getting saved in :  ",'blue')+colored( args.path,'red' ))
         sys.stdout = open(args.path, "w")
-        images_file_cnt.get_img_cnt(args.url)
+        img_cnt_depth.get_url_depth.get_img_cnt(args.url,args.depth)
         sys.stdout.close()
     elif args.e == 'cnt_image':
-        images_file_cnt.get_img_cnt(args.url)
+        img_cnt_depth.get_url_depth.get_img_cnt(args.url,args.depth)
 
     if args.e == "source_code" and args.edit == "save":
         print(colored("source code for this url getting saved in :  ",'blue')+colored( args.path,'red' ))
@@ -104,26 +100,25 @@ def find(args):
     if args.e == "help":
         print(colored("BASIC WEB CRAWLER WITH FOLLOWING FUNCTIONS: \n",'red')
                                     + colored("[head]        - give header of url  \n", 'green')
-                                    + colored("[link_d1]     - links for depth1  of url \n", 'green')
-                                    + colored("[phone_no]    - give phone numbers in given url \n", 'green')
-                                    + colored("[cnt_image]   - count number of image in givel url \n", 'green')
-                                    + colored("[email]       - give emails present in given url \n" , 'green')
+                                    + colored("[phone_no]    - give phone numbers in url \n", 'green')
+                                    + colored("[cnt_image]   - count number of image in url \n", 'green')
+                                    + colored("[email]       - give emails present in url \n" , 'green')
                                     + colored("[link]        - give links for any depth \n", 'green')
-                                    + colored("[source_code] - give source code of url \n", 'green')
+                                    + colored("[source_code] - give source code of given url \n", 'green')
                                     + colored("[web_text]    - give web text data of url \n", 'green')
-                                    + colored("[ss]          - give take screen shot of url \n", 'green'))
+                                    + colored("[ss]          - give take screen shot of given url \n", 'green'))
 
 
 parser = argparse.ArgumentParser()
 #arguments-url
-parser.add_argument( '--e' , type = str, help = colored(" Enter: [head] / [ss] / [link_d1]/[phone_no]/[cnt_image]/[email]/[link]/[source_code]/[web_text]/[help] ",'red'))
+parser.add_argument( '--e' , type = str, help = colored(" Enter: [head] / [ss] /[phone_no]/[cnt_image]/[email]/[link]/[source_code]/[web_text]/[help] ",'red'))
                                                                                   
 
-parser.add_argument( '--url' , type = str, help = colored(" give target url here ",'blue'))
-parser.add_argument( '--depth' , type = int ,default=0 , help = colored(" GIVE DEPTH HERE (by default it will take 0)",'blue') + colored(" will only take for [link] ",'red'))
+parser.add_argument( '--url' , type = str, help = colored(" GIVE TARGET URL HERE ",'blue'))
+parser.add_argument( '--depth' , type = int ,default=0 , help = colored(" GIVE DEPTH HERE   ",'blue') + colored(" deault value is 0",'red'))
 
 #arguement-file
-parser.add_argument( '--create' , type = str, help =colored('directory or txt file','blue' ))
+parser.add_argument( '--create' , type = str, help =colored('dir or txt file','blue' ))
 parser.add_argument( '--name' , type = str, help =colored('enter file or directory name','blue' ))
 parser.add_argument( '--path' , type = str, help =colored('enter path','blue' ))
 parser.add_argument( '--edit' , type = str, help =colored('save your data to text file','blue'))
@@ -131,4 +126,5 @@ parser.add_argument( '--edit' , type = str, help =colored('save your data to tex
 args = parser.parse_args()
 
 sys.stdout.write(str(find(args)))
+
 
