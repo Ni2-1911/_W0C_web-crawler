46edc7645f8b70d3c93fe90ac2291506148cf60f
Nitu Kumari <79151737+Ni2-1911@users.noreply.github.com>
2021-04-10 23:43:01 +0530

    Update _recurssion_.py

--- _recurssion_.py
+++ _recurssion_.py
@@ -12,26 +12,34 @@ def get_links(u):
     global ux,x
     x=[]
     for links in u:
-        try:
+        try :
             if("http" not in links and "https" not in links and links[0]== "/" and links != ""):
                 links=ux+links
             elif("http" not in links and "https" not in links and links[0] !="/" and links != ""):
                 links=ux+"/"+links
-        except:
-            pass
-        
+        except TypeError :
+            pass    
+
+
         try:
-            response = requests.get(links)
+            header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0", 
+                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8", 
+                    "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", 
+                    "Connection": "close", "Upgrade-Insecure-Requests": "1"}  
+        
+            response = requests.get(links, headers = header)
             soup = BeautifulSoup(response.text,'html.parser')
+
             for link in soup.find_all('a'):
                 x.append(link.get('href'))
         except:
             pass
 
     print(x)
-    print("no of links we get: " , len(x))
+    print("no of links we get: " , colored (len(x),'red'))
     print("time :" , colored (time.time() - initial_time,'red'))
     return(x)
+
 
 def get_depth(d,url):
     text=[]
@@ -43,14 +51,15 @@ def get_depth(d,url):
     else: 
         print(colored("PLEASE ENTER A VALID DEPTH ",'red'))
 
+
 def get_url_depth(u ,d):
     url=[]
     url.append(u)
     ux=url[0]
     depth = int(d)
     get_depth(depth,url)
-
-
+    
+   
 #example
 #get_url_depth('https://google.com',1)
 
